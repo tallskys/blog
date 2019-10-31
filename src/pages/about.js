914b7633aa83147ce847e3d72c533f4b970d2c3b
tallskys <tallsky89@gmail.com>
2019-10-31 11:30:11 -0400

    slight about page change

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutPage = () => (
       
         <h1>About Me</h1>
         <p>
-            I am a professional web developer with a knack for clean and simple designs. I also have a passion for all things writing including technical, fiction, and everything in between. If you need work done in any of those areas and want it swiftly and professionally, contact me at me@tannercarri.co
+            I am a professional web developer with a knack for clean and simple designs. I also have a passion for all things writing including technical, fiction, and everything in between. If you need work done in any of those areas and want it swiftly and professionally, you can reach me here: me@tannercarri.co
         </p>
     </AboutLayout>
 )
